7ac170f5094e150e430853000821517462f90479
Fourteen98 <emuhiydeen6@gmail.com>
2023-03-26 09:11:14 +0000

    add chapter8

---
+++ Chapter9/Lab9/lab9a_txt.js
@@ -0,0 +1,21 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Lab 9
+
+      Project to read field values from web storage
+      Author: 
+      Date:   
+
+*/
+
+/* Page Objects */
+
+let riderName = document.getElementById("riderName");
+let ageGroup = document.getElementById("ageGroup");
+let bikeOption = document.getElementById("bikeOption");
+let routeOption = document.getElementById("routeOption");
+let accOption = document.getElementById("accOption");
+let region = document.getElementById("region");
+let miles = document.getElementById("miles");
+let comments = document.getElementById("comments");
---
+++ Chapter9/Lab9/lab9b_txt.js
@@ -0,0 +1,23 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Lab 9
+
+      Project to read field values from session storage
+      Author: 
+      Date:   
+
+*/
+
+/* Page Objects */
+
+let riderName = document.getElementById("riderName");
+let ageGroup = document.getElementById("ageGroup");
+let bikeOption = document.getElementById("bikeOption");
+let routeOption = document.getElementById("routeOption");
+let accOption = document.getElementById("accOption");
+let region = document.getElementById("region");
+let miles = document.getElementById("miles");
+let comments = document.getElementById("comments");
+
+
